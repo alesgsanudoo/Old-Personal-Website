0f0c35f004ea9fdd7c62001ba61718a1c4361c89
alesgsanudoo_ <alejandrosanudo10@gmail.com>
2023-01-05 20:55:03 -0500

    Added reload

--- js/language.js
+++ js/language.js
@@ -1,11 +1,48 @@
-function toggleEmoji(language) {
-    let txt = language.innerText;
-    if (txt === "🇺🇸 EN") {
-        toggleLanguage("Spanish")
-    } else {
-        toggleLanguage("English")
+function toggleLanBt(len) {
+    if (len === "default" || len == null || len.length === 0) {
+        if (localStorage.length === 0) {
+            localStorage.setItem("lan", "english")
+            toggleEmoji(true)
+        } else {
+            let lan = localStorage.getItem("lan")
+            if (lan === "english") {
+                localStorage.removeItem("lan")
+                localStorage.setItem("lan", "english")
+                toggleEmoji(true)
+            } else if (lan === "spanish") {
+                localStorage.removeItem("lan")
+                localStorage.setItem("lan", "spanish")
+                toggleEmoji(true)
+            }
+        }
     }
-    language.innerText = txt === '🇺🇸 EN' ? '🇪🇸 ES' : '🇺🇸 EN';
+}
+
+window.onload = toggleLanBt("default")
+
+function toggleEmoji(option) {
+    let language = document.getElementById("language")
+    if (!option) {
+        if (language.innerText === "🇺🇸 EN") {
+            toggleLanguage("Spanish")
+            localStorage.removeItem("lan")
+            localStorage.setItem("lan", "spanish")
+            language.innerText = "🇪🇸 ES"
+        } else if (language.innerText === "🇪🇸 ES") {
+            toggleLanguage("English")
+            localStorage.removeItem("lan")
+            localStorage.setItem("lan", "english")
+            language.innerText = "🇺🇸 EN"
+        }
+    } else {
+        if (localStorage.getItem("lan") === "spanish") {
+            toggleLanguage("Spanish")
+            language.innerText = "🇪🇸 ES"
+        } else if (localStorage.getItem("lan") === "english") {
+            toggleLanguage("English")
+            language.innerText = "🇺🇸 EN"
+        }
+    }
 }
 
 function toggleLanguage(language) {
@@ -17,12 +54,13 @@ function toggleLanguage(language) {
     let project_title = document.getElementById("project-title")
     let project1_description = document.getElementById("project1-description")
     let footer = document.getElementById("footer")
+    let notfound = document.getElementById("tittle-not-found")
     if (language === "Spanish") {
         nav_contact.innerHTML = "Contactar <i class=\"fa-regular fa-envelope\"></i>"
         about_title.innerHTML = "Sobre mí"
-        if (screen.width < 600){
+        if (screen.width < 600) {
             about_status.innerHTML = " Ingeniería Informática | Purdue University"
-        } else  {
+        } else {
             about_status.innerHTML = " Ingeniería Informática | Purdue University"
         }
         description.innerHTML = "(ES)Anited hipaid outte phrolo. Hisour the lignea thow anetwo by somplat ins this hadmin vels.\n" +
@@ -32,6 +70,7 @@ function toggleLanguage(language) {
         bt_resume.innerHTML = "Currículum"
         project_title.innerHTML = "Mis projectos"
         footer.innerHTML = "&copy; Derechos de Autor 2022, Alejandro Griffith"
+        notfound.innerHTML = " Página no encontrada"
     } else {
         nav_contact.innerHTML = "Contact <i class=\"fa-regular fa-envelope\"></i>"
         about_title.innerHTML = "About Me"
@@ -43,5 +82,6 @@ function toggleLanguage(language) {
         bt_resume.innerHTML = "resume"
         project_title.innerHTML = "My projects"
         footer.innerHTML = "&copy; Copyright 2022, Alejandro Griffith"
+        notfound.innerHTML = "Page not found"
     }
 }
